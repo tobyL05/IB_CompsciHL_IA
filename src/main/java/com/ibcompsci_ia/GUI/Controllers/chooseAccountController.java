863f1a5e27c6f3e6d3f249ad2386589857e83bb0
tobyL05 <toby123mc@gmail.com>
2022-04-11 07:57:31 +0700

    fixes in chooseAccountController

--- src/main/java/com/ibcompsci_ia/GUI/Controllers/chooseAccountController.java
+++ src/main/java/com/ibcompsci_ia/GUI/Controllers/chooseAccountController.java
@@ -7,18 +7,26 @@ import com.ibcompsci_ia.GUI.Models.chooseAccountModel;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class chooseAccountController {
 
     @FXML private Button backBtn;
     @FXML private VBox accountVBox;
+    @FXML private Label noAccountsTxt;
     private chooseAccountModel model;
 
     public chooseAccountController(){
         model = new chooseAccountModel();
-        if(!model.checkAccountsFound()){
+        checkAccounts(model.checkAccountsFound());
+    }
+
+    private void checkAccounts(boolean acc){
+        if(!acc){
             System.out.println("No accounts");
+        }else{
+            System.out.println("Accoutns found");
         }
     }
 
