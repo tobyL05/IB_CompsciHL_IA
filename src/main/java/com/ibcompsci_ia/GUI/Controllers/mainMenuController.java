4f6a4df15c8f67811b58ce39d5cdf37204c8b993
tobyL05 <toby123mc@gmail.com>
2022-07-02 13:21:43 +0700

    updates

--- src/main/java/com/ibcompsci_ia/GUI/Controllers/mainMenuController.java
+++ src/main/java/com/ibcompsci_ia/GUI/Controllers/mainMenuController.java
@@ -1,17 +1,22 @@
 package com.ibcompsci_ia.GUI.Controllers;
 
-import java.io.IOException;
-
 import com.ibcompsci_ia.Main;
 import com.ibcompsci_ia.GUI.Models.mainMenuModel;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class mainMenuController {
 
     mainMenuModel model;
-    @FXML private Label userText;
-    @FXML private Label pwdText;
+    @FXML private Button darkModeBtn;
+    @FXML private Button bibleBtn;
+    @FXML private Button searchBtn;
+    @FXML private Button bookmarkBtn;
+    @FXML private Button notepadBtn;
+    @FXML private Button logoutBtn;
+    @FXML private Label verseTxt;
+
 
 
     public mainMenuController(){
@@ -21,9 +26,29 @@ public class mainMenuController {
     @FXML
     public void initialize(){
     }
+
+    @FXML
+    private void bibleBtnPress(){
+        //switch to bible
+    }
+
+    @FXML
+    private void searchBtnPress(){
+        //switch to search scene
+    }
     
     @FXML
-    private void switchToSecondary() throws IOException {
-        Main.setRoot("loginPage");
+    private void bookmarkBtnPress(){
+        //switch to bookmarks scene
+    }
+
+    @FXML
+    private void notepadBtnPress(){
+        //switch to notepad
+    }
+
+    @FXML
+    private void logoutPress(){
+        //logout
     }
 }
