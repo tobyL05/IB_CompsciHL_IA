ca485addcb1a483b7bb069b31c6509eb4314e8ea
tobyL05 <toby123mc@gmail.com>
2022-07-19 12:10:07 +0700

    new user file

--- src/main/java/com/ibcompsci_ia/users/Session.java
+++ src/main/java/com/ibcompsci_ia/users/Session.java
@@ -1,8 +1,10 @@
 package com.ibcompsci_ia.users;
 
 public class Session {
+	private User user;
 	//has a user, reads/writes properties	
 	public Session(String fileName){
+		
 		//make reader/writer for file
 	}
 }
---
+++ src/main/java/com/ibcompsci_ia/users/User.java
@@ -0,0 +1,28 @@
+package com.ibcompsci_ia.users;
+
+import java.util.ArrayList;
+
+//assign to a properties file
+public class User {
+	private String user;
+	private String pwd;
+	private String notepadPath;
+	private ArrayList<String> savedRefs;
+
+	public User(String user, String pwd){
+		this.user = user;
+		this.pwd = pwd;
+		//new acc
+	}
+
+	public User(String filename){
+		//existing acc
+	}
+
+	//assign notepadpath
+	//assign savedversespath;
+	//
+
+
+	
+}
