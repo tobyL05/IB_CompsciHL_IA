04bfb32ff8e5554b166513b87d572b5411ded80c
tobyL05 <toby123mc@gmail.com>
2023-02-06 00:25:43 +0700

    convert findChap to inputstream

--- src/main/java/com/ibcompsci_ia/parser/CSVParser.java
+++ src/main/java/com/ibcompsci_ia/parser/CSVParser.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -11,7 +13,8 @@ import com.ibcompsci_ia.Enums.paths;
 
 public class CSVParser {
 	
-	private final String bookData = getClass().getResource(paths.resourcePath.toString() + "books.csv").getPath();
+	//private final String bookData = getClass().getResource(paths.resourcePath.toString() + "books.csv").getPath();
+	private final InputStream bookDataStream = getClass().getResourceAsStream(paths.resourcePath.toString() + "books.csv");
 	public static ArrayList<String> books = new ArrayList<String>();
 	public static ArrayList<String> idBooks = new ArrayList<String>();
 	public static HashMap<String, Integer> bookMap = new HashMap<>(); //(bookName, no of chaps)
@@ -19,7 +22,8 @@ public class CSVParser {
 	public CSVParser(){
 		try{
 			String line = "";
-			BufferedReader br = new BufferedReader(new FileReader(bookData));
+			//BufferedReader br = new BufferedReader(new FileReader(bookData));
+			BufferedReader br = new BufferedReader(new InputStreamReader(bookDataStream));
 			while((line = br.readLine()) != null){
 				String[] bookdata = line.split(",");
 				CSVParser.books.add(bookdata[0]); //eng
@@ -34,5 +38,10 @@ public class CSVParser {
 		}catch(IOException ioe){
 			System.out.println("io exception");
 		}
+	}
+	public static void main(String[] args) {
+		new CSVParser();
+		System.out.println(CSVParser.books);
+		
 	}
 }
--- src/main/java/com/ibcompsci_ia/parser/findChapter.java
+++ src/main/java/com/ibcompsci_ia/parser/findChapter.java
@@ -1,10 +1,15 @@
 package com.ibcompsci_ia.parser;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -17,28 +22,49 @@ public class findChapter {
 	private Document doc;
     private static Document staticDoc;
     private String bookName;
-	private final String dirPath = getClass().getResource(paths.htmlPath.toString()).getPath();
-    private final static String staticDirPath = findChapter.class.getResource(paths.htmlPath.toString()).getPath();
+	private final String dirPathStr = paths.htmlPath.toString();
+	private final InputStream dirIs = getClass().getResourceAsStream(paths.htmlPath.toString());
     private ArrayList<String> verses = new ArrayList<>();
     private int chapSize;
 
+    //public findChapter(String bookName) throws IOException{
+        //this.bookName = bookName;
+        //BufferedReader br = new BufferedReader(new InputStreamReader(dirIs));
+        //String line="";
+        //while((line = br.readLine()) != null){
+            //System.out.println(line);
+        //}
+        ////removeSpaces();
+    //}
+
     public findChapter(String bookName) throws IOException{
-        this.bookName = bookName;
+        int bookNum = CSVParser.books.indexOf(bookName) + 1;
+        String bookNostr = "";
+        if(bookNum < 10){
+            bookNostr = "0" + Integer.toString(bookNum);
+        }else{
+            bookNostr = Integer.toString(bookNum);
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(dirIs));
+        String line="";
+        String filePath = dirPathStr;
+        InputStream file;
+        System.out.println(bookNostr);
+        while((line = br.readLine()) != null){
+            //System.out.println(line);
+            if(!line.contains("files") && line.startsWith(bookNostr)){
+                filePath += line;
+                System.out.println(filePath);
+                file = getClass().getResourceAsStream(filePath);
+                doc = Jsoup.parse(file,"UTF-8",""); //works
+            }
+        }
         removeSpaces();
     }
 
     //somehow return current book and chapter
 
     private void removeSpaces() throws IOException{
-        File dir = new File(dirPath);
-        File[] book = dir.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File file){
-                return file.getName().contains(bookName.replace(" ",""));
-            }
-        });
-        //System.out.println("File name: " + book[0].getName());
-        doc = Jsoup.parse(new File(dirPath + book[0].getName()),"windows-1252",dirPath + book[0].getName());
         Elements sup = doc.select("sup");
         for(Element sups:sup){
             sups.replaceWith(new TextNode(" "));
@@ -88,128 +114,20 @@ public class findChapter {
         return false;
     }
 
-    private static boolean staticIsVerse(String text){
-        int ints = 0;
-        int letters = 0;
-        char[] textArr = text.toCharArray();
-        for(Character c:textArr){
-            if(Character.isDigit(c)){
-                ints++;
-            }else{
-                letters++;
-            }
-            if(letters - ints == 5){
-                return true;
-            }
-        }
-        return false;
-    }    
-
     public int getChapSize(){
         return chapSize;
     }
 
-    public static ArrayList<String> staticgetVerseinChapter(String bookName, String chapNo){
-        ArrayList<String> verses = new ArrayList<>();
-        File dir = new File(staticDirPath);
-        File[] book = dir.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File file){
-                return file.getName().contains(bookName.replace(" ","")) && !file.isDirectory();
-            }
-        });
-        //for(File f:book){
-            //System.out.println(f.getName());
+    //private static void testAll(){
+        //new CSVParser();
+        //for(int i = 0;i < CSVParser.books.size();i++){ //for each book
+            //System.out.println(CSVParser.books.get(i));
+            //for(int j = 0;j < CSVParser.bookMap.get(CSVParser.books.get(i));j++){ //for each chapter in the book
+                ////System.out.println(CSVParser.books.get(i) + " " + (j+1) + " size: " + findChapter.getChapSize(CSVParser.books.get(i), Integer.toString(j+1)));
+                //assert findChapter.getChapSize(CSVParser.books.get(i), Integer.toString(j+1)) != 0;
+            //}
         //}
-        //System.out.println(book[0].getName());
-        try{
-            staticDoc = Jsoup.parse(new File(staticDirPath + book[0].getName()),"UTF-8");
-        }catch(IOException io){
-            System.out.println("Cannot access book html");
-        }
-        Elements sup = staticDoc.select("sup");
-        for(Element sups:sup){
-            sups.replaceWith(new TextNode(" "));
-        }
-        Element table;
-        try{
-            table = staticDoc.select("table").get(1); //get the table
-        }catch(IndexOutOfBoundsException e){
-           table = staticDoc.select("table").get(0); //get the table
-        }
-        Elements rows = table.select("tr"); //get all rows
-        //  System.out.println(table);
-        for(Element row:rows){ // for each row
-            //System.out.println(row);
-            Elements col = row.select("td"); //get table data
-            if(col.get(1).text().equalsIgnoreCase(bookName + " " + chapNo+1)){//if end of chapter
-                break;
-            }
-            if(staticIsVerse(col.get(1).text()) && col.get(1).text().startsWith((Integer.toString(Integer.parseInt(chapNo) + 1) + ":"))){
-                verses.add(col.get(1).text() + "\n");
-            }
-        }
-        return verses;
-
-    }
-    
-    public static int getChapSize(String bookName, String chapNo){
-        int chapSize = 1;
-        File dir = new File(staticDirPath);
-        //System.out.println(bookName);
-        File[] book = dir.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File file){
-                return file.getName().contains(bookName.replace(" ","")) && !file.isDirectory();
-            }
-        });
-        //for(File f:book){
-            //System.out.println(f.getName());
-        //}
-        //System.out.println(book[0].getName());
-        try{
-            staticDoc = Jsoup.parse(new File(staticDirPath + book[0].getName()),"UTF-8");
-        }catch(IOException io){
-            System.out.println("Cannot access book html");
-        }
-        Elements sup = staticDoc.select("sup");
-        for(Element sups:sup){
-            sups.replaceWith(new TextNode(" "));
-        }
-        Element table;
-        try{
-            table = staticDoc.select("table").get(1); //get the table
-        }catch(IndexOutOfBoundsException e){
-            table = staticDoc.select("table").get(0); //if it doesnt work get the other table
-        }
-        Elements rows = table.select("tr"); //get all rows
-        //  System.out.println(table);
-        for(Element row:rows){ // for each row
-            //System.out.println(row);
-            Elements col = row.select("td"); //get table data
-            if(col.get(1).text().equalsIgnoreCase(bookName + " " + chapNo+1)){
-                break;
-            }
-            if(staticIsVerse(col.get(1).text()) && col.get(1).text().startsWith(chapNo + ":")){
-                //System.out.println(col.get(1).text());
-                //System.out.println(chapSize);
-                chapSize++;
-            }
-        }
-        return chapSize-1;
-    }
-
-
-    private static void testAll(){
-        new CSVParser();
-        for(int i = 0;i < CSVParser.books.size();i++){ //for each book
-            System.out.println(CSVParser.books.get(i));
-            for(int j = 0;j < CSVParser.bookMap.get(CSVParser.books.get(i));j++){ //for each chapter in the book
-                //System.out.println(CSVParser.books.get(i) + " " + (j+1) + " size: " + findChapter.getChapSize(CSVParser.books.get(i), Integer.toString(j+1)));
-                assert findChapter.getChapSize(CSVParser.books.get(i), Integer.toString(j+1)) != 0;
-            }
-        }
-    }
+    //}
 
     /**
      * Says hello to the world.
@@ -217,7 +135,8 @@ public class findChapter {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        findChapter fc = new findChapter("Genesis");
-        System.out.println(fc.getVersesinChapter(1, 0));
+        new CSVParser();
+        findChapter fc = new findChapter("1 John");
+        //System.out.println(fc.getVersesinChapter(1, 0));
     }
 }
